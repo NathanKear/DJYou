7ce4d089a68d095337cd2b5547e240b6a4f9c97d
Nathan Kear <n.j.kear@gmail.com>
2018-03-24 22:58:27 +1300

    Add routing

--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import App from './components/app';
+import UserWelcome from './components/user_welcome';
+import SessionCreate from './components/session_create';
+import VideoView from './components/video_view';
+import VideoAdd from './components/video_add';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/session/create" component={SessionCreate}/>
+          <Route path="/:sessionId/view" component={VideoView}/>
+          <Route path="/:sessionId/add" component={VideoAdd}/>
+          <Route path="/" component={UserWelcome}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
